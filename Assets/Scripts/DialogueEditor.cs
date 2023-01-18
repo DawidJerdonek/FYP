afb113e84c925477ad8ebb6aca7d0c93e90f25ae
DawidJerdonek <dawid.jerdonek@gmail.com>
2023-01-18 21:44:05 +0000

    Created a basic blueprint for dialogue system

---
+++ Assets/Scripts/DialogueEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueEditor : MonoBehaviour
+{
+    public GameObject dialogueEditor;
+    public GameObject observerCharacter;
+    public GameObject missionControlCharacter;
+  
+
+    public TextMeshProUGUI editable;
+    public TextMeshProUGUI greetingBotText;
+    public TextMeshProUGUI characterName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        greetingBotText.text = editable.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
 public class PlayerController : MonoBehaviour
 {
-
     public SteamVR_Action_Vector2 input;
     private float speed = 2;
-    private CharacterController characterController;    
+    private CharacterController characterController;
+
+    public GameObject dialogueCanvas;
+    public DialogueEditor dialogueSystem;
+    public TextMeshProUGUI textToDisplay;
 
     // Start is called before the first frame update
     void Start()
@@ -22,5 +26,55 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(input.axis.x, 0, input.axis.y));
         characterController.Move(speed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up) - new Vector3(0,9.81f,0) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Observer")
+        {
+            dialogueSystem.characterName.text = "The Observer";
+            dialogueCanvas.SetActive(true);
+           
+        }
+
+        if (other.tag == "MissionControl")
+        {
+            dialogueSystem.characterName.text = "Mission Control";
+            dialogueCanvas.SetActive(true);
+        }
+
+        if (other.tag == "GreetingBot")
+        {
+            dialogueSystem.characterName.text = "Greeting Bot";
+            dialogueSystem.greetingBotText.text = dialogueSystem.editable.text;
+            textToDisplay.text = dialogueSystem.greetingBotText.text;
+            dialogueCanvas.SetActive(true);
+            Debug.Log("Bot");
+        }
+    }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.tag == "Observer")
+    //    {
+    //        dialogueCanvas.enabled = true;
+    //        Debug.Log("collision stay");
+    //    }
+    //}
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Observer")
+        {
+            dialogueCanvas.SetActive(false);
+        }
+        if (other.tag == "MissionControl")
+        {
+            dialogueCanvas.SetActive(false);
+        }
+        if (other.tag == "GreetingBot")
+        {
+            dialogueCanvas.SetActive(false);
+        }
     }
 }
