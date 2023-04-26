b1447d0b9e3335b79dccb677a3f340fd0d3a1c7b
DawidJerdonek <dawid.jerdonek@gmail.com>
2023-04-26 19:29:43 +0100

    Added CRC cards to TDD and deleted garbage from project

--- Assets/Scripts/StageGrabber.cs
+++ Assets/Scripts/StageGrabber.cs
@@ -5,16 +5,4 @@ using UnityEngine;
 public class StageGrabber : MonoBehaviour
 {
     public int stageValue;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
